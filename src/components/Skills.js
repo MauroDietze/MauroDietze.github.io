9ef5b2c1c47c2f1039ed9e2e1eed48adc8580426
MauroDietze <mauroid123@gmail.com>
2024-08-15 12:29:57 -0300

    fix Skills title

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -22,7 +22,7 @@ const Skills = () => {
 
     return (
         <>
-            <h2 className='font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32'>{t('skills.title')}</h2>
+            <h2 className='font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32'>{t('about.skills.title')}</h2>
             <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark
             lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] 
             lg:bg-circularLightLg lg:dark:bg-circularDarkLg
