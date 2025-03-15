b9b971da3f74ea86639c1b0cb0adb930b7286d3b
MauroDietze <mauroid123@gmail.com>
2025-03-15 14:06:51 -0300

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,67 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 
-const { fontFamily } = require("tailwindcss/defaultTheme");
+const {fontFamily} =require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class",
-  safelist: [
-    {
-      pattern: /animation-delay-/,
-    },
-    {
-      pattern: /animation-duration-/,
-    }
-  ],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        mont: ["var(--font-mont)", ...fontFamily.sans],
+        mont: ['var(--font-mont)', ...fontFamily.sans]
       },
       colors: {
         dark: "#1b1b1b",
         light: "#f5f5f5",
         primary: "#B63E96",
-        primaryDark: "#58E6D9",
-      },
-      keyframes: {
-        "move-x": {
-          "0%": { transform: "translateX(var(--move-x-from, -100%))" },
-          "100%": { transform: "translateX(var(--move-x-to, calc(100% + 100vw)))" },
-        },
+        primaryDark: "#58E6D9"
       },
       animation: {
-        "move-x": "move-x linear infinite",
+        'spin-slow': 'spin 8s linear infinite',
       },
+      scrollBehavior: [
+        'smooth'
+      ],
+      backgroundImage: {
+        circularLight: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px, #f5f5f5 100px);',
+        circularDark: 'repeating-radial-gradient(rgba(255,255,255,0.5) 2px, #1b1b1b 8px, #1b1b1b 100px);',
+        circularLightLg: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px, #f5f5f5 80);',
+        circularDarkLg: 'repeating-radial-gradient(rgba(255,255,255,0.5) 2px, #1b1b1b 8px, #1b1b1b 80px);',
+        circularLightMd: "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 60px)",
+        circularDarkMd: "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 6px,#1b1b1b 60px)",
+        circularLightSm: "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 40px)",
+        circularDarkSm: "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 4px,#1b1b1b 40px)",
+      }
     },
     screens: {
       "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
       xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
       lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
       md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
       sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
       xs: { max: "479px" },
+      // => @media (max-width: 479px) { ... }
     },
   },
-  plugins: [
-    require("@tailwindcss/aspect-ratio"),
-    function ({ addUtilities }) {
-      const newUtilities = {};
-      for (let i = 1; i <= 20; i++) {
-        newUtilities[`.animation-delay-${i}`] = {
-          "animation-delay": `-${i * 2}s`,
-        };
-      }
-      addUtilities(newUtilities, ["responsive", "hover"]);
-    },
-    function ({ addComponents }) {
-      addComponents({
-        ".animation-move-x": {
-          animation: "move-x linear infinite",
-        },
-      });
-    }
-  ]  
-};
+  plugins: [],
+}
